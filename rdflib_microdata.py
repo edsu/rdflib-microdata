5b6c219733b3e88948d9c48ede8629dba83d88a7
Ed Summers <ehs@pobox.com>
2011-06-09 10:36:20 +0000

    cleaned out some old code from before microdata module

--- rdflib_microdata.py
+++ rdflib_microdata.py
@@ -1,6 +1,4 @@
-import xml.dom.minidom
-
-from html5lib import HTMLParser, treebuilders
+import microdata
 
 from rdflib import URIRef
 from rdflib.plugin import register
@@ -11,18 +9,5 @@ register("microdata", Parser, "rdflib_microdata", "MicrodataParser")
 class MicrodataParser(Parser):
 
     def parse(self, source, sink, **kwargs):
-        parser = HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
-        tree = parser.parse(source)
-        self._build_graph(tree, sink, 0)
-
-    def _build_graph(self, e, sink, depth):
-        print e, depth
-        #if isinstance(e, xml.dom.minidom.Element):
-        #    if e.hasAttribute('itemscope'):
-        #        if e.hasAttribute('itemtype'):
-        #            print e.getAttribute('itemtype')
-
-        for child in e.childNodes:
-            self._build_graph(child, sink, depth + 1)
-
-        #sink.add((URIRef("foo"), URIRef("bar"), URIRef("baz")))
+        items = microdata._build_graph(source)
+        # build the graph now :-)
